44e39232c480f6a99c6825f3284fa4aebbeeabbf
Joneswc <joneswc@gmail.com>
2019-07-06 16:52:04 -0300

    list and detail components

--- src/app/closet/closet-routing.module.ts
+++ src/app/closet/closet-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ClothesListComponent} from './containers/clothes-list/clothes-list.component';
+import {ClothesDetailComponent} from './containers/clothes-detail/clothes-detail.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'list' },
+  { path: 'list', component: ClothesListComponent },
+  { path: 'detail', component: ClothesDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/closet/closet.module.ts
+++ src/app/closet/closet.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ClosetRoutingModule } from './closet-routing.module';
+import { ClothesListComponent } from './containers/clothes-list/clothes-list.component';
+import { ClothesDetailComponent } from './containers/clothes-detail/clothes-detail.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ClothesListComponent, ClothesDetailComponent],
   imports: [
     CommonModule,
     ClosetRoutingModule
