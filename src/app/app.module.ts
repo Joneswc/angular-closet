39291976d19d0c6459473e55fef980938c3e2f20
Joneswc <joneswc@gmail.com>
2019-07-13 14:24:31 -0300

    install animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {ClosetModule} from './closet/closet.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import {ClosetModule} from './closet/closet.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ClosetModule
+    ClosetModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
