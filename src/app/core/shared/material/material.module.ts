45ef96c3b9532dba3afafc347a2f4a9aa341dba4
Joneswc <joneswc@gmail.com>
2019-07-13 15:56:18 -0300

    clean unused import

--- src/app/core/shared/material/material.module.ts
+++ src/app/core/shared/material/material.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import {MatButtonModule, MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule} from '@angular/material';
 
 
